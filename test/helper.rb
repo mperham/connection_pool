7de3eafa1c6b80c9494f88bdaf890cd06cc4a66a
Damian Janowski <damian.janowski@gmail.com>
2013-08-14 23:35:43 -0300

    Clean up unused method.

--- test/helper.rb
+++ test/helper.rb
@@ -1,17 +1,6 @@
-require 'rubygems'
 require 'minitest/pride'
 require 'minitest/autorun'
 
 $VERBOSE = 1
 
 require_relative '../lib/connection_pool'
-
-class Minitest::Unit::TestCase
-
-  def async_test(time=0.5)
-    q = TimedQueue.new
-    yield Proc.new { q << nil }
-    q.timed_pop(time)
-  end
-
-end
