d2bd545e9a674d3532a1e064d4895447094c1112
Tero Tasanen <tero.tasanen@gmail.com>
2018-10-20 09:32:46 -0400

    Remove wrapper for monotonic time
    This is included in all recent MRI and JRUBY versions so the wrapper is
    not needed anymore.

--- lib/connection_pool/timed_stack.rb
+++ lib/connection_pool/timed_stack.rb
@@ -1,6 +1,5 @@
 require 'thread'
 require 'timeout'
-require_relative 'monotonic_time'
 
 ##
 # Raised when you attempt to retrieve a connection from a pool that has been
@@ -74,7 +73,7 @@ class ConnectionPool::TimedStack
     options, timeout = timeout, 0.5 if Hash === timeout
     timeout = options.fetch :timeout, timeout
 
-    deadline = ConnectionPool.monotonic_time + timeout
+    deadline = ::Process.clock_gettime(Process::CLOCK_MONOTONIC) + timeout
     @mutex.synchronize do
       loop do
         raise ConnectionPool::PoolShuttingDownError if @shutdown_block
@@ -83,7 +82,7 @@ class ConnectionPool::TimedStack
         connection = try_create(options)
         return connection if connection
 
-        to_wait = deadline - ConnectionPool.monotonic_time
+        to_wait = deadline - ::Process.clock_gettime(Process::CLOCK_MONOTONIC)
         raise Timeout::Error, "Waited #{timeout} sec" if to_wait <= 0
         @resource.wait(@mutex, to_wait)
       end
