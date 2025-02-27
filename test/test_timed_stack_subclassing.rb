93a48af839ec341a97389f3eb8baf2d486fa2818
Jason King <jk@radical.gd>
2025-02-27 14:55:24 -0600

    Tests for `try_fetch_connection` override

---
+++ test/test_timed_stack_subclassing.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require_relative "helper"
+
+class TestTimedStackSubclassing < Minitest::Test
+  def setup
+    @klass = Class.new(ConnectionPool::TimedStack)
+  end
+
+  def test_try_fetch_connection
+    obj = Object.new
+    stack = @klass.new(1) { obj }
+    assert_equal false, stack.send(:try_fetch_connection)
+    assert_equal obj, stack.pop
+    stack.push obj
+    assert_equal obj, stack.send(:try_fetch_connection)
+  end
+
+  def test_override_try_fetch_connection
+    obj = Object.new
+
+    stack = @klass.new(1) { obj }
+    stack.push stack.pop
+
+    connection_stored_called = "cs_called"
+    stack.define_singleton_method(:connection_stored?) {|*| raise connection_stored_called }
+    e = assert_raises { stack.send(:try_fetch_connection) }
+    assert_equal connection_stored_called, e.message
+
+    stack.define_singleton_method(:try_fetch_connection) { fetch_connection() }
+    assert_equal obj, stack.send(:try_fetch_connection)
+  end
+end
